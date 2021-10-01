fc3cc33825c14aefc11b9e04d40c1377a4105375
Dario Ramos <52785865+darks000@users.noreply.github.com>
2021-10-01 12:57:43 -0500

    agregar tarjeta visual y lista tarjetas

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TarjetaCreditoComponent implements OnInit {
 
+  listTarjetas : any[]=[
+   { titulo: 'dario', tarjeta: '32323', fechaexpiracion: '25/05',cvv: '012'},
+   { titulo: 'juancho', tarjeta: '32323', fechaexpiracion: '2/05',cvv: '042'},
+   { titulo: 'juaniot', tarjeta: '313', fechaexpiracion: '25/2',cvv: '032'}
+  ];
   constructor() { }
 
   ngOnInit(): void {
